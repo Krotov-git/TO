9538a36aadd266588d04b22c999e908108cabbd9
Mikhail-codeway <krotov.ne@gmail.com>
2021-03-11 00:02:08 +0300

    Доработанный ГУИ

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 # подгружаю необходимые библиотеки
 from PyQt5.QtWidgets import QMainWindow, QGridLayout, QLabel, \
-    QPushButton, QCheckBox, QApplication, QComboBox, QLineEdit, QCompleter, QWidget, QMessageBox
+    QPushButton, QCheckBox, QApplication, QComboBox, QLineEdit, QCompleter, QWidget, \
+    QMessageBox, QTableWidget, QTableWidgetItem, QTableView, QHeaderView, QHBoxLayout, QFormLayout, QGroupBox, \
+    QVBoxLayout
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QIcon
 import sys
@@ -28,7 +30,7 @@ class TravelWindow(QMainWindow):
     # функция с элементами графического интерфейса
     def init_gui(self):
 
-        # элемент позволяющий вводить/выводить текст
+        # элемент позволяющий вводить/выводить данные
         self.cost = QLineEdit()
         self.calc_value = QLineEdit()
 
@@ -51,50 +53,76 @@ class TravelWindow(QMainWindow):
         button_raschet = QPushButton('Рассчитать')
         button_raschet.clicked.connect(self.calc_cost)
 
+        # кнопка вывести данные
+        button_table = QPushButton('Вывести данные')
+        button_table.clicked.connect(self.spisok_data_to)
+
         self.msgBox = QMessageBox()
+
+        # таблица данных ТО
+        self.table = QTableWidget()
+        #self.table.setFixedHeight(250)
+        #self.table.setFixedWidth(200)
+        self.table.setColumnCount(4)
+        self.table.setRowCount(len(self.data))
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.table.setHorizontalHeaderLabels(["Имя ТО", "USD", "EUR", "Дата"])
 
         # сеточный макет отвечающий за расположение элементов внутри графического интерфейса и их отображение
         grid = QGridLayout()
 
     # сами элементы с их названием и координатами расположения
+
+        # первая координата, строка в сетке
+        # вторая координата, столбец в сетке
+        # третья координата, на сколько строк растягивается элемент
+        # четвертая координата, на сколько столбцов растягивается элемент
+
         # строка ввода названия туроператора
-        grid.addWidget(QLabel('Введите название туроператора:'), 1, 0, 1, 4)
-        grid.addWidget(self.company, 2, 0, 1, 4)
+        grid.addWidget(QLabel('Введите имя ТО:'), 1, 0, 1, 5)
+        grid.addWidget(self.company, 2, 0, 1, 5)
 
         # "выпадающий список" с озможностью выбора туроператора без ввода текста
-        grid.addWidget(QLabel('Выберите туроператора из списка:'), 3, 0, 1, 4)
-        grid.addWidget(self.combo, 4, 0, 1, 4)
+        grid.addWidget(QLabel('Выберите имя ТО:'), 3, 0, 1, 5)
+        grid.addWidget(self.combo, 4, 0, 1, 5)
 
         # элемент выбора валюты
-        grid.addWidget(QLabel('USD'), 5, 0)
-        grid.addWidget(self.check_usd, 5, 1)
+        grid.addWidget(QLabel('USD'), 5, 0, 1, 2)
+        grid.addWidget(self.check_usd, 6, 0, 1, 2)
 
         # элемент выбора валюты
-        grid.addWidget(QLabel('EUR'), 5, 2)
-        grid.addWidget(self.check_eur, 5, 3)
+        grid.addWidget(QLabel('EUR'), 5, 2, 1, 4)
+        grid.addWidget(self.check_eur, 6, 2, 1, 4)
 
         # строка ввода данных в у.е для пересчета их в рубли
-        grid.addWidget(QLabel('Введите данные в у.е:'), 6, 0, 1, 4)
-        grid.addWidget(self.cost, 7, 0, 1, 4)
+        grid.addWidget(QLabel('Введите данные в у.е:'), 7, 0, 1, 5)
+        grid.addWidget(self.cost, 8, 0, 1, 5)
 
         # строка выводящая значение пересчитанное из у.е в рубли
-        grid.addWidget(QLabel('Сумма в рублях:'), 8, 0, 1, 4)
-        grid.addWidget(self.calc_value, 9, 0, 1, 4)
+        grid.addWidget(QLabel('Сумма в рублях:'), 9, 0, 1, 5)
+        grid.addWidget(self.calc_value, 10, 0, 1, 5)
 
         # кнопка при нажатии на которую происходит расчет
-        grid.addWidget(button_raschet, 10, 0)
+        grid.addWidget(button_raschet, 11, 0, 1, 5)
+
+        # кнопка при нажатии на которую происходит вывод данных то в таблицу
+        grid.addWidget(button_table, 11, 6, 1, 1)
+
+        # таблица данных ТО
+        grid.addWidget(QLabel('Данных курсов ТО и дата, для сверки'), 1, 6, 1, 1)
+        grid.addWidget(self.table, 2, 6, 9, 1)
 
         central_widget = QWidget()
         central_widget.setLayout(grid)
         self.setCentralWidget(central_widget)
 
         # устанавливаем параметры размера окна и его расположения при запуске программы
-        self.setGeometry(300, 300, 300, 320)
+        self.setGeometry(300, 300, 390, 300)  # первые два значения расположение окна программы н экране при запуске
+                                              # последние два значения ширина и высота окна программы
 
         # логотип программы в левом верхнем углу
         self.setWindowIcon(QIcon('LT.png'))
         self.show()
-
 
     def changeText(self, index):
         self.company.setText(self.combo.itemText(index))
@@ -106,12 +134,47 @@ class TravelWindow(QMainWindow):
         money = 0
 
         if self.check_usd.checkState() == 2:
-                money = self.data[comp][0] #usd
+            self.check_eur.setChecked(False)
+            money = self.data[comp][0] #usd
         if self.check_eur.checkState() == 2:
-                money = self.data[comp][1] #eur
+            self.check_usd.setChecked(False)
+            money = self.data[comp][1] #eur
 
-        value = round(float(value) * float(money), 2)
-        self.calc_value.setText(str(value))
+        try:
+            if float(value) >= 0:
+                value = round(float(value) * float(money), 2)
+                self.calc_value.setText(str(value))
+            else:
+                self.calc_value.setText("Введено отрицательное число!")
+        except:
+            self.calc_value.setText("Введены НЕ числовые данные!")
+
+    # функция заполняет таблицу данными ТО
+    def spisok_data_to(self):
+        indicator_name = 0   # счетчик, участвует в добавление названия ТО каждый раз в новую строку
+        indicator_usd = []  # список, выполняет роль счетчика, участвует в добавление курса usd ТО каждый раз в новую строку
+        indicator_eur = []  # список, выполняет роль счетчика, участвует в добавление курса eur ТО каждый раз в новую строку
+        indicator_date = []  # список, выполняет роль счетчика, участвует в добавление даты курса ТО каждый раз в новую строку
+        for i in self.data:
+            self.table.setItem(indicator_name, 0, QTableWidgetItem(i))
+            indicator_name += 1
+
+            indicator_if = 0  # счетчик, условия добавления данных
+            js = []   # список, данных ТО, по индексу выбираем usd, eur, date
+            for j in self.data[i]:
+                js.append(j)
+                if indicator_if == 0:
+                    self.table.setItem(len(indicator_usd), 1, QTableWidgetItem(str(js[0])))
+                    indicator_usd.append(j)
+                if indicator_if == 1:
+                    self.table.setItem(len(indicator_eur), 2, QTableWidgetItem(str(js[1])))
+                    indicator_eur.append(j)
+                if indicator_if == 2:
+                    jss = js[2]
+                    self.table.setItem(len(indicator_date), 3, QTableWidgetItem(str(jss[-5:])))
+                    indicator_date.append(j)
+                indicator_if += 1
+
 
     # функция вызывает окно с вопросом о закрытии программы при попытки звкрыть ее нажав на крестик
     def closeEvent(self, event):
@@ -123,13 +186,11 @@ class TravelWindow(QMainWindow):
 
 
 if __name__ == '__main__':
-    company_list = ['My company 1',
-                    'My company 2',
-                    'Your company 1']
-    usd = 75
-    eur = 95
 
+    company_list = {'key': 'My company 1'}
+    company_list = {'My company 1': [70, 85]}
+
+    window = TravelWindow(company_list)
     app = QApplication(sys.argv)
-    window = TravelWindow(company_list, usd, eur)
 
     app.exec_()
--- SQL_Lite_DB/DB.py
+++ SQL_Lite_DB/DB.py
@@ -9,8 +9,6 @@ class DataBase:
         self.date = date.today()
 
     def set_values_TO(self):
-        j = Common_Parser()
-        i = j.get_new_data()
 
         connection = sqlite3.connect("SQL_Lite_DB/database.db")
         cursor = connection.cursor()
@@ -23,9 +21,9 @@ class DataBase:
         self.spisok_now_date = []
 
         for row in rows:
-            table_date = row[3]
+            self.table_date = row[3]
             #print('table_date==> ', table_date)
-            self.spisok_table_date.append(table_date)
+            self.spisok_table_date.append(self.table_date)
             self.spisok_now_date.append(now_date)
 
         # print('spisok_table_date==> ', self.spisok_table_date)
@@ -33,6 +31,8 @@ class DataBase:
         # print(self.spisok_now_date != self.spisok_table_date)
 
         if self.spisok_now_date != self.spisok_table_date:
+            j = Common_Parser()
+            i = j.get_new_data()
             cursor.execute("DELETE FROM spisok_TO")
             for item in i.items():
                 self.info.append(item[0])
@@ -74,49 +74,5 @@ if __name__ == '__main__':
     DaBa.get_values_TO()
     print('return!', DaBa.get_values_TO())
 
-
-
-
-
-
-# # создаю класс для парсинга данных с сайта
-# class DataBase:
-#
-#     # задаю атрибуты класса
-#     def __init__(self):
-#        # self.site = 'https://pay.travel/site_controller_courses/index/?date='
-#         self.site = 'https://www.tez-tour.com/'
-#         self.data = self.parse_site()
-#
-#     # функция осуществляющая непосредственный парсинг данных с сайта
-#     def parse_site(self):
-#         # now = datetime.datetime.now()
-#         # date = now.strftime("%Y-%m-%d")
-#         #site = self.site + date
-#         site = 'https://www.tez-tour.com/'
-#         headers = {
-#             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
-#         }
-#
-#         response = requests.get(site, headers=headers)
-#         soup = BeautifulSoup(response.text, features='lxml')
-#         trs = soup.findChildren("table")
-#
-#         # создаю саму базу данных по типу словаря где ключ название туроператора, а курс значение ключа
-#         db = {}
-#         i = 0
-#         for tr in trs[2]:
-#             i += 1
-#             if i == 4:
-#                 tds = tr.findAll("td")
-#                 name = 'TezTour'
-#                 usd = tds[1].text
-#                 eur = tds[2].text
-#                 db[name] = [usd[:-3], eur[:-3]]
-#
-#         return db
-#
-#     def get_data(self):
-#         return self.data
 
 
